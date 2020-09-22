49f0c5072ca402e8bbe234a2276fb95fcc6ac5fd
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-09-22 18:59:17 +0800

    Making sure DragPIN random works

--- DragPIN/views.py
+++ DragPIN/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .forms import Profileform, Drag
 from .models import Profile
+import random
 # Create your views here.
 
 
@@ -18,13 +19,22 @@ def Drag_up (request):
     return render(request, 'DragPIN_sign_up.html', context={'form': form, })
 
 def Drag_in(request):
+    charlist = ['A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E']
+    random.shuffle(charlist)
+    row1 = charlist
+    random.shuffle(row1)
+    row2 = row1
+    random.shuffle(row2)
+    row3 = row2
+    random.shuffle(row3)
+    row4 = row3
     results = [
-                [{"id": "00", "value": "A"}, {"id": "01", "value": "B"}, {"id": "02", "value": "C"}, {"id": "03", "value": "D"}, {"id": "04", "value": "E"},{"id": "05", "value": "A"},{"id": "06", "value": "B"}, {"id": "07", "value": "C"}, {"id": "08", "value": "D"}, {"id": "09", "value": "E"}],
-                [{"id": "10", "value": "A"}, {"id": "11", "value": "B"}, {"id": "12", "value": "C"}, {"id": "13", "value": "D"}, {"id": "14", "value": "E"},{"id": "15", "value": "A"},{"id": "16", "value": "B"}, {"id": "17", "value": "C"}, {"id": "18", "value": "D"}, {"id": "19", "value": "E"}],
-                [{"id": "20", "value": "A"}, {"id": "21", "value": "B"}, {"id": "22", "value": "C"}, {"id": "23", "value": "D"}, {"id": "24", "value": "E"},{"id": "25", "value": "A"},{"id": "26", "value": "B"}, {"id": "27", "value": "C"}, {"id": "28", "value": "D"}, {"id": "29", "value": "E"}],
-                [{"id": "30", "value": "A"}, {"id": "31", "value": "B"}, {"id": "32", "value": "C"}, {"id": "33", "value": "D"}, {"id": "34", "value": "E"},{"id": "35", "value": "A"},{"id": "36", "value": "B"}, {"id": "37", "value": "C"}, {"id": "38", "value": "D"}, {"id": "39", "value": "E"}],
+                [{"id": "00", "value": row1[0]}, {"id": "01", "value": row1[1]}, {"id": "02", "value": row1[2]}, {"id": "03", "value": row1[3]}, {"id": "04", "value": row1[4]},{"id": "05", "value": row1[5]},{"id": "06", "value": row1[6]}, {"id": "07", "value": row1[7]}, {"id": "08", "value": row1[8]}, {"id": "09", "value": row1[9]}],
+                [{"id": "10", "value": row2[0]}, {"id": "11", "value": row2[1]}, {"id": "12", "value": row2[2]}, {"id": "13", "value": row2[3]}, {"id": "14", "value": row2[4]},{"id": "15", "value": row2[5]},{"id": "16", "value": row2[6]}, {"id": "17", "value": row2[7]}, {"id": "18", "value": row2[8]}, {"id": "19", "value": row2[9]}],
+                [{"id": "20", "value": row3[0]}, {"id": "21", "value": row3[1]}, {"id": "22", "value": row3[2]}, {"id": "23", "value": row3[3]}, {"id": "24", "value": row3[4]},{"id": "25", "value": row3[5]},{"id": "26", "value": row3[6]}, {"id": "27", "value": row3[7]}, {"id": "28", "value": row3[8]}, {"id": "29", "value": row3[9]}],
+                [{"id": "30", "value": row4[0]}, {"id": "31", "value": row4[1]}, {"id": "32", "value": row4[2]}, {"id": "33", "value": row4[3]}, {"id": "34", "value": row4[4]},{"id": "35", "value": row4[5]},{"id": "36", "value": row4[6]}, {"id": "37", "value": row4[7]}, {"id": "38", "value": row4[8]}, {"id": "39", "value": row4[9]}]
                 ]
-                
+
     if request.method == 'POST':
         form = Drag(request.POST)
         arr = request.POST.getlist('result[]')
