712724fc47c80ff2b546d5ba7c25b93bb24b8428
mahrous-amer <eng.mahrous.amer@gmail.com>
2020-11-22 17:41:59 +0800

    more db

--- g_auth/settings.py
+++ g_auth/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'g_auth.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 #default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 # }
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
